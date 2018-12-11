ef208b709e822cc5584b2ab9488e95dbed902836
khandurideepak2007 <khandurideepak2007@gmail.com>
2018-12-11 12:13:08 +0530

    Add files via upload

---
+++ hello/CHANGELOG.md
@@ -0,0 +1,4 @@
+## 0.0.1
+
+* TODO: Describe initial release.
+# 12/11/2018 - Github repo updated..
---
+++ hello/example/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+#import <hello/HelloPlugin.h>
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [HelloPlugin registerWithRegistrar:[registry registrarForPlugin:@"HelloPlugin"]];
+}
+
+@end
